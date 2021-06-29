ed821a3122c444d72d0600a825ddafcbe6c44131
BrianSammit <briansammit.cruz@gmail.com>
2021-06-29 16:47:50 -0500

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import React from 'react';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
 
 function App() {
   return (
-    <div className="App">
-      <h3>Hello world</h3>
-    </div>
+    <>
+      <Display />
+      <ButtonPanel />
+    </>
   );
 }
 
